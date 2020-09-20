2350d54a4ade3e56d374985d8614b357d0079ea6
Yash Gupta <18guptayash@gmail.com>
2020-09-20 15:09:36 +0530

    Extra

--- Islands_graph_matrix.cpp
+++ Islands_graph_matrix.cpp
@@ -10,6 +10,7 @@ bool isSafe(int M[][COL], int row, int col, bool vis[ROW][COL]){
         return (row >= 0) && (row < ROW) && (col >= 0) && (col < COL) && (M[row][col] && !vis[row][col]); 
     }
     
+    //USING DFS
     void dfs_islands(int M[][COL], bool vis[][COL], int row,int col){
         int rows[]={-1,-1,-1, 0,  1, 1, 1 ,0 };
         int cols[]={1 , 0 ,-1,-1,-1, 0, 1, 1 };
@@ -24,6 +25,42 @@ bool isSafe(int M[][COL], int row, int col, bool vis[ROW][COL]){
         return;
         
     }   
+
+    //USING BFS
+    void BFS(int mat[R][COL], bool vis[R][COL], 
+         int si, int sj) 
+    { 
+  
+    // These arrays are used to get row and 
+    // column numbers of 8 neighbours of 
+    // a given cell 
+    int row[] = { -1, -1, -1, 0, 0, 1, 1, 1 }; 
+    int col[] = { -1, 0, 1, -1, 1, -1, 0, 1 }; 
+  
+    // Simple BFS first step, we enqueue 
+    // source and mark it as visited 
+    queue<pair<int, int> > q; 
+    q.push(make_pair(si, sj)); 
+    vis[si][sj] = true; 
+  
+    // Next step of BFS. We take out 
+    // items one by one from queue and 
+    // enqueue their univisited adjacent 
+    while (!q.empty()) { 
+  
+        int i = q.front().first; 
+        int j = q.front().second; 
+        q.pop(); 
+  
+        // Go through all 8 adjacent 
+        for (int k = 0; k < 8; k++) { 
+            if (isSafe(mat, i + row[k],j + col[k], vis)) { 
+             vis[i + row[k]][j + col[k]] = true; 
+             q.push(make_pair(i + row[k], j + col[k])); 
+            } 
+        } 
+    } 
+    } 
     
     int  countIslands(int M[][COL]){
         bool vis[ROW][COL];
@@ -46,6 +83,8 @@ bool isSafe(int M[][COL], int row, int col, bool vis[ROW][COL]){
         // cout<<count;
         return count;
     }
+
+
 int main(){
 
 //No of islands
---
+++ ajo2_ladder_A.cpp
@@ -0,0 +1,27 @@
+#include<map>
+#include<string>
+#include<stack>
+#include<list>
+#include<unordered_map>
+#include<queue>
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	unsigned int w;
+	cin>>w;
+	if(w==2){
+		cout<<"NO";
+	}
+	else if(w%2==0){
+		cout<<"YES";
+	}
+
+	else{
+		cout<<"NO";
+	}
+
+	return 0;
+}
