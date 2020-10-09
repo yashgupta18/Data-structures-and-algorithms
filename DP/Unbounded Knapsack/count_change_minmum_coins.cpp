dc8870f3116b03cfc1262d6b115b578834f903bb
Yash Gupta <18guptayash@gmail.com>
2020-10-09 15:01:36 +0530

    coin change 2 questions

---
+++ DP/Unbounded Knapsack/count_change_minmum_coins.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+int min(int a, int b) 
+{ 
+    return (a < b) ? a : b; 
+} 
+
+
+int coin_change_count(int arr[], int sum, int n) 
+{ 
+    int i, j; 
+    int t[n + 1][sum + 1]; 
+    
+    for (int i = 1; i < n+1; i++) 
+        t[i][0] = 0; 
+
+    //first row set  int_max
+    for (int i = 0; i < sum+1; i++) 
+        t[0][i] = INT_MAX-1; 
+
+    //for setting 2nd row(only this ques exception) check notes
+    for (int j = 1; j < sum+1; ++j)
+    {
+        if((j%arr[0])==0){
+            t[1][j]=j%arr[0];
+        }
+        else{
+            t[i][j]=INT_MAX-1;
+        }
+    }
+    //for understanding
+    for (i = 1; i <= n; i++) { 
+        for (j = 1; j <= sum; j++) { 
+            t[i][j]=0;
+        } 
+    }
+
+    cout<<"Before DP"<<endl;
+    for (i = 0; i <= n; i++) { 
+        for (j = 0; j <= sum; j++) { 
+            cout<<t[i][j]<<" ";
+        } 
+        cout<<endl;
+    } 
+    cout<<endl;
+
+    
+    for (int i = 1; i <= n; i++) { 
+        for (int j = 1; j <= sum; j++) { 
+            if(arr[i-1]<=j){
+
+                //add one if the coin is selected
+                t[i][j]=min( t[i][j-arr[i-1]] + 1 , t[i-1][j] );
+            }
+            else{
+                //just use the above value from table
+                t[i][j]= t[i-1][j];
+
+            }
+
+        } 
+    }
+    cout<<endl;
+
+    // for understanding
+    cout<<"After DP"<<endl;
+    for (i = 0; i <= n; i++) { 
+        for (j = 0; j <= sum; j++) { 
+            cout<<t[i][j]<<" ";
+        } 
+        cout<<endl;
+    } 
+    cout<<endl; 
+    
+    cout<<"Final Ans"<<endl;
+    cout<<"Minimum number of coins="<<" ";
+    return t[n][sum]; 
+}
+
+int main(){
+	int arr[] ={ 1,2,3 };
+    int sum = 5; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+	cout<<coin_change_count(arr,sum,n);
+
+
+}
