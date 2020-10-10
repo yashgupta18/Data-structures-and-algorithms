a620a73ef9349f3b634e3dec92096caaf7dfc77a
Yash Gupta <18guptayash@gmail.com>
2020-10-10 16:50:03 +0530

    Shortest Common Subsequence

---
+++ DP/Longest common subsequence/LCS_Shortest_common_SuperSequence.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int max(int a, int b) 
+{ 
+    return (a > b) ? a : b; 
+} 
+
+void LCS(string X, string Y, int m, int n){
+	int t[m+1][n+1];
+
+	for (int i = 0; i < m+1; ++i)
+	{
+		for (int j = 0; j < n+1; ++j){
+			if(i==0 || j==0){
+				t[i][j]=0;
+			}
+			else{
+				t[i][j]=-1;
+			}
+		}
+	}
+
+
+	cout<<"Before DP"<<endl;
+	for (int i = 0; i < m+1; ++i)
+	{
+		for (int j = 0; j < n+1; ++j)
+		{
+			cout<<t[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+	
+	for (int i = 1; i < m+1; ++i)
+	{
+		for (int j = 1; j < n+1; ++j)
+		{
+			if(X[i-1]==Y[j-1]){ 
+				//if last char is same reduce size of both strings
+				t[i][j] = 1+t[i-1][j-1];
+			}
+			else{
+				//if last is not same see for both by decreasing size of each and choosing the max 
+				t[i][j] = max(t[i-1][j], t[i][j-1]);
+			}
+		}
+	}
+
+	cout<<"After DP"<<endl;
+	for (int i = 0; i < m+1; ++i)
+	{
+		for (int j = 0; j < n+1; ++j)
+		{
+			cout<<t[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
+
+	int count=0;
+	cout<<"Length of Shortest Common SuperSequence = m+n-LCS of X & Y = ";
+	cout<< m+n-t[m][n]<<endl ;
+	return;
+}
+
+int main(){
+	string X="AGGTAB";
+	int m=X.length();
+	string Y="GXTXAYB";
+	int n=Y.length();
+	LCS(X,Y,m,n);
+}
