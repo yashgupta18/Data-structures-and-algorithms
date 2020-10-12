02fb62619211df70b90cec529124bb2e1ff365d7
Yash Gupta <18guptayash@gmail.com>
2020-10-12 17:57:13 +0530

    Sequence pattern matching

--- DP/Longest common subsequence/Sequence_pattern_matching.cpp
+++ DP/Longest common subsequence/Sequence_pattern_matching.cpp
@@ -6,26 +6,21 @@ int max(int a, int b)
     return (a > b) ? a : b; 
 } 
 
+
 void LCS(string X, string Y, int m, int n){
 	int t[m+1][n+1];
-
 	for (int i = 0; i < m+1; ++i)
 	{
-		for (int j = 0; j < n+1; ++j){
-			if(i==0){
-				t[i][j]=1;
-			}
-
-			else{
-				t[i][j]=0;
-			}
-			
-			
+		for (int j = 0; j < n+1; ++j)
+		{
+			 if(i==0 || j==0){
+			 	t[i][j]=0;
+			 }
+			 else{
+			 	t[i][j]=-1;
+			 }
 		}
-
-
 	}
-
 
 	cout<<"Before DP"<<endl;
 	for (int i = 0; i < m+1; ++i)
@@ -38,20 +33,15 @@ void LCS(string X, string Y, int m, int n){
 	}
 	cout<<endl;
 
-	
 	for (int i = 1; i < m+1; ++i)
 	{
-		for (int j = 1; j < n+1; ++j)
+		for (int j= 1; j < n+1; ++j)
 		{
-			if(X[i-1]==Y[j-1]){ 
-				//if char at pos X[i-1]==Y[j-1], increase the count 
-				t[i][j] = t[i-1][j] + t[i][j-1];
-
+			if(X[i-1]==Y[j-1]){
+				t[i][j]=1+t[i-1][j-1];
 			}
-	
 			else{
-				//if it is not same see for both by decreasing size of each and choosing the max 
-				t[i][j] =t[i][j-1];
+				t[i][j]= max(t[i-1][j], t[i][j-1]);
 			}
 		}
 	}
@@ -67,14 +57,25 @@ void LCS(string X, string Y, int m, int n){
 	}
 	cout<<endl;
 
-	cout<<"Longest subsequence = "<< t[m][n];	
-	return;
+	cout<<t[m][n]<<endl;
+
+	//if length of LCS is same as length of string X, then X is subsequence of 
+	if(t[m][n]==m){
+		cout<<"true";
+		return;
+	}
+	else{
+		cout<<"False";
+		return;
+	}
 }
 
 int main(){
-	string X="rabbbit";
+	// Q= is string X subsequence of Y or not?
+	string X="AXY";
 	int m=X.length();
-	string Y="rabbit";
+	string Y="ADXCPY";
 	int n=Y.length();
 	LCS(X,Y,m,n);
+	return 0;
 }
