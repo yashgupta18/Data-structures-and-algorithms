4a2f145ce3a863017551fc7e8964431d8003fb76
Yash Gupta <18guptayash@gmail.com>
2020-10-22 18:53:19 +0530

    K closest numbers

---
+++ Heap/K_closest_numbers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <queue> 
+using namespace std; 
+
+
+void k_largest(int arr[], int n, int k, int X){
+	priority_queue<pair<int, int> > max_heap;
+	vector<int> sub;
+	for (int i = 0; i < n; ++i)
+		{
+			max_heap.push(make_pair(abs(arr[i]-X), arr[i]));
+			if(max_heap.size()>k){
+				max_heap.pop();
+			}
+		}
+
+	cout<<"Differnce\t"<< "Number in array"<<endl;
+	while(!max_heap.empty()){
+
+		cout<<max_heap.top().first<<"\t"<<"\t"<<max_heap.top().second<<endl;
+		max_heap.pop();
+	}
+	
+	return;
+}
+
+
+int main(){
+
+	int arr[]={5,6,7,8,9};
+	int n=sizeof(arr)/sizeof(arr[0]);
+	int k=3;
+	int X=7;
+	k_largest(arr,n,k,X);
+
+	return 0;
+}
