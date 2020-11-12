f241913b19adbbd79d08b2145456b2714a6259a3
Yash Gupta <18guptayash@gmail.com>
2020-11-12 23:19:36 +0530

    SPOJ string changes

--- SPOJ/stringTask.cpp
+++ SPOJ/stringTask.cpp
@@ -6,34 +6,36 @@
 #include<map>
 #include<string>
 #include<cstring>
+#include<regex>
 
-void deleteString(string &s){
-	string vowels='aeiouy'
-	for(i=0; i<s.size(); i++){
-		if(v.find(s[i])){
-			s.dele
-		}
-	}
+using namespace std;
+
+
+string deleteString(string str){
+	 regex r("[aeiouyAEIOUY]");  
+      
+    return regex_replace(str, r, ""); 
 }
 
 string insertCon(string s){
-	string newString = '';
-	for (int i = 0; i < s.length(); i=i+2)
+	string newString="";
+	for (int i = 0; i < s.length(); ++i)
 	{
-		newString.append(s[i]);
-		newString.append('.');
+		newString=newString+"."+s[i];
 	}
 	return newString;
 }
 
-void convertToUC(string &newString){
-	transform(newString.begin(), newString.end(), newString.begin(), ::toupper); 
+void convertToUC(string &newString2){
+	transform(newString2.begin(), newString2.end(), newString2.begin(), ::tolower); 
 	return;
 }
 
 int main(){
-	string s= 'hellloo';
-	deleteString(s);
-	string newString=insertCon(s);
-	convertToUC(newString);
+	string s;
+	cin>>s;
+	string newString1=deleteString(s);
+	string newString2=insertCon(newString1);
+	convertToUC(newString2);
+	cout<<newString2;
 }
