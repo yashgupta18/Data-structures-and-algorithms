b68e037d1415b154f1535af466683241021787f4
Yash Gupta <18guptayash@gmail.com>
2020-11-23 19:59:01 +0530

    pract

--- pract2.c
+++ pract2.c
@@ -1,15 +1,52 @@
-// #include <iostream>
-// using namespace std; 
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-   #include<stdio.h>
-   #include<stdlib.h>
-   #include<string.h>                              
- 
-  int main()
-   {
-       char *ptr;
-       ptr = (char*)malloc(sizeof(char)*11);
-       strcpy(ptr,"sanfoundry");
-       printf("%d\n",*ptr);
-       return 0;
+bool char_valid_check(char symbol) {
+   return (symbol == 'a') || (symbol == 'b');
+}
+
+bool string_check(char *string, int len) {
+   short state = 0;
+   int i; char symbol;
+
+   for(i = 0; i < len; i++) {
+       symbol = *(string + i);
+      
+       if(char_valid_check(symbol) == false) {
+           printf("Invalid character found.\n");
+           return false;
+       }
+
+       if(state == 0 && symbol == 'a') {
+           state = 1;
+       }
+       else if(state == 0 && symbol == 'b') {
+           state = 0;
+       }
+       else if(state == 1 && symbol == 'a') {
+           state = 2;
+       }
+       else if(state == 1 && symbol == 'b') {
+           state = 0;
+       }
+       else if(state == 2) {
+           break;
+       }
    }
+   return state == 2;
+}
+
+
+int main() {
+   char *input_string; bool valid; int len;
+
+   printf("Input string: ");
+   scanf("%s", input_string);
+
+   len = strlen(input_string);
+   valid = string_check(input_string, len);
+
+   puts(valid ? "String accepted": "Not accepted");
+   return 0;
+}
