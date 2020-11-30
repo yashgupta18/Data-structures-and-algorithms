dfd6977eace64474f48b2493fe89762e05c1deab
Yash Gupta <18guptayash@gmail.com>
2020-11-30 19:46:33 +0530

    segment tree updation

--- SEGMENT TREE/segmentTree.cpp
+++ SEGMENT TREE/segmentTree.cpp
@@ -30,6 +30,7 @@ void buildTree(int l, int r, int arr[],int segArr[], int pos){
 	buildTree(l,mid, arr,segArr,(2*pos)+1 );
 	buildTree(mid+1,r, arr,segArr,(2*pos)+2 );
 	segArr[pos]=segArr[(2*pos)+1] + segArr[(2*pos)+2];
+	// segArr[pos]=segArr[(2*pos)+1] + segArr[(2*pos)+2];
 }
 
 int getSum(int l, int r, int ql, int qr,int segArr[], int pos){
@@ -41,33 +42,51 @@ int getSum(int l, int r, int ql, int qr,int segArr[], int pos){
 	return getSum(l, mid, ql, qr, segArr, (2*pos)+1) + getSum(mid+1, r, ql, qr, segArr, (2*pos)+2);
 }
 
+void updateVal(int segArr[],int l, int r, int P, int pos, int val){
+	if(l==r){
+		segArr[pos]=val;
+		return;
+	} 
+
+	int mid=(l+r)/2;
+	if(P<=mid){
+		updateVal(segArr,l,mid,P, 2*pos+1, val);
+	}
+	else{
+		updateVal(segArr,mid+1,r,P, 2*pos+2, val);
+	}
+	segArr[pos]=segArr[2*pos+1]+segArr[2*pos+2];
+}
+
 
 int main(){
-	// Node *root= newNode(1);
-	// root->left=newNode(2);
-	// root->right=newNode(3);
-	// root->left->right=newNode(4);
-	// root->right->left=newNode(5);
-	// root->right->right=newNode(6);
-	// root->right->left->left=newNode(7);
 	
 	int arr[]= {1,3,-2,8,-7};
 	int n=sizeof(arr)/sizeof(arr[0]);
 	int segArr[4*n];
+	
 	buildTree(0,n-1, arr,segArr, 0);
+	
 	int n2=sizeof(segArr)/sizeof(segArr[0]);
 	for (int i = 0; i <= 9; ++i)
 	{
-		cout<<i<<" ";
+		// cout<<i<<" ";
 	}
 	cout<<endl;
 	for (int i = 0; i < 9; ++i)
 	{
-		// cout<<segArr[i]<<" ";
+		cout<<segArr[i]<<" ";
 	}
 
 	int ans=getSum(0,n-1, 1,3, segArr, 0);
-	cout<<ans;
+	// cout<<ans;
+	cout<<endl;
+	updateVal(segArr, 0,n-1, 4, 0, -6);
+	cout<<"After Updation"<<endl;
+	for (int i = 0; i < 9; ++i)
+	{
+		cout<<segArr[i]<<" ";
+	}
 	return 0;
 
 }
