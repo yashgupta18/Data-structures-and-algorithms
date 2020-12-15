a1949ce0bfb10b926bb33c3a7c132cb82ac7f1a2
Yash Gupta <18guptayash@gmail.com>
2020-12-15 15:43:58 +0530

    virtual fn

---
+++ OOPS/virtual.cpp
@@ -0,0 +1,26 @@
+#include<iostream> 
+using namespace std; 
+  
+class Base 
+{ 
+   int x; 
+public: 
+    virtual void fun() = 0; 
+    int getX() { return x; } 
+}; 
+  
+// This class inherits from Base and implements fun() 
+class Derived: public Base 
+{ 
+    int y; 
+public: 
+    void fun() { cout << "fun() called"; } 
+}; 
+  
+int main(void) 
+{ 
+    Derived d; 
+    d.fun();
+    cout<<d.getX(); 
+    return 0; 
+} 
