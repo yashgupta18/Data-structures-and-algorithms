31af91ce7eea5d8d2fabf6c281a22b66291d732f
Yash Gupta <18guptayash@gmail.com>
2021-01-08 16:40:43 +0530

    rotate matrix 90 deg

--- 450 questions/Matrix/51_rotate90.cpp
+++ 450 questions/Matrix/51_rotate90.cpp
@@ -30,7 +30,7 @@ using namespace std;
 #define int             long long
 #define ll 				long long int
 #define ld				long double
-#define endl			"\n"
+// #define endl			"\n"
 #define as(arr)         sizeof(arr)/sizeof(arr[0]);
 #define pb              push_back
 #define pii             pair<ll,ll>
@@ -58,7 +58,7 @@ using namespace std;
 // mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
  
 // typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-#define M 4 
+#define M 4
 #define N 4
 // struct compare
 // {
@@ -80,45 +80,52 @@ void file_i_o()
 }
 
 
-int kSmallest(int mat[M][N], int k){
-	//declare min heap with (int , (i,j)) structure
-	priority_queue< pair<int,pair<int, int> >, vector<pair<int, pair<int, int> > >, greater<pair<int,pair<int, int> > > > min_heap;
-	
-	//insert first column
+void rotate90Clockwise(int mat[M][N]){
+	// transpose
 	for (int i = 0; i < M; ++i)
 	{
-		/* code */
-		min_heap.push(make_pair(mat[i][0], make_pair(i,0)));
+		for (int j = i; j < N; ++j)
+		{
 
-	}
-	int count=7, ans;
-
-	// keepinserting next column element of current top and pop
-	while(count--){
-		int tempi=min_heap.top().second.first;
-		int tempj=min_heap.top().second.second;
-		ans=min_heap.top().first;
-		min_heap.pop();
-		min_heap.push(make_pair(mat[tempi][tempj+1], make_pair(tempi, tempj+1)));
+			swap(mat[i][j], mat[j][i]);
+		}
 	}
 
-	return ans;
-	
+	//swap in each col
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0, k=M-1; j < M, k>0; ++j, k--)
+		{
+
+			/* code */
+			swap(mat[j][i], mat[k][i]);
+		}
+	}
+
+
+	for (int i = 0; i < M; ++i)
+	{
+		for (int j = 0; j < N; ++j)
+		{
+
+			cout<<mat[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+
 }
-
 
 int32_t main()
 {
     clock_t begin = clock();
 
-   int mat[M][N] = { {10, 20, 30, 40}, 
-                    {15, 25, 35, 45}, 
-                    {24, 29, 37, 48}, 
-                    {32, 33, 39, 50}, 
-                };  
-  	int k=7;
-    cout<<kSmallest(mat, k);  
-
+    //EXPLAINATION:  https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2471/Very-concise-O(log(min(MN)))-iterative-solution-with-detailed-explanation
+    int mat[M][N] = { { 1, 2, 3, 4 },
+                      { 5, 6, 7, 8 },
+                      { 9, 10, 11, 12 },
+                      { 13, 14, 15, 16 } };
+    rotate90Clockwise(mat);
+    // printMatrix(arr);
 	#ifndef ONLINE_JUDGE 
 	  clock_t end = clock();
 	  cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC*1000<<" ms";
