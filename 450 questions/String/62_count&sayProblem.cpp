b716a7f62d2993670258cb9faf830fc14b72fcf6
Yash Gupta <18guptayash@gmail.com>
2021-01-12 18:22:37 +0530

    count nd say problem

---
+++ 450 questions/String/62_count&sayProblem.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+
+string countnndSay(int n) {
+    if (n == 0) return "";
+    string res = "1";
+    while (--n) {
+        string cur = "";
+        for (int i = 0; i < res.size(); i++) {
+            int count = 1;
+             while ((i + 1 < res.size()) && (res[i] == res[i + 1])){
+                count++;    
+                i++;
+            }
+            cur += to_string(count) + res[i];
+        }
+        res = cur;
+    }
+    return res;
+}
+
+
+int main() 
+{ 
+	// https://leetcode.com/problems/count-and-say/discuss/16043/C%2B%2B-solution-easy-understand
+	// pattern
+	//1
+	// 11
+	// 21
+	// 1211
+	// 111221
+	// 312211
+    int N = 9; 
+    cout << countnndSay(N) << endl; 
+    return 0; 
+} 
