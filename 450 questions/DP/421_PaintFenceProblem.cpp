d230b52a221276fc4fa548d1ef0b82acd5fbd208
Yash Gupta <18guptayash@gmail.com>
2021-01-28 21:35:38 +0530

    updates

--- 450 questions/DP/421_PaintFenceProblem.cpp
+++ 450 questions/DP/421_PaintFenceProblem.cpp
@@ -10,18 +10,22 @@ int paintFence(int n, int c){
     //this problem initially for min n=2.So calculate same and diff for n=2
     int same=c*1;
     int diff=c*(c-1);
+    int total=same+diff;
     // then from 3 to n calculate using prev same and diff
     for (int i = 3; i < n; ++i)
     {
        same=diff*1;
-       diff=same+diff;
+       diff=total*(c-1);
+       total=same+diff;
     }
-    return same+diff;
+    return total;
 	
 }
 
 
 int main(){
+    // https://www.youtube.com/watch?v=ju8vrEAsa3Q&ab_channel=Pepcoding
+    // https://www.geeksforgeeks.org/painting-fence-algorithm/
 	int n=2;
     int c=4;
 	cout<<paintFence(n, c);
