4e0cdd2c914ef36e40ee73a5143ab7ce1fb25177
Yash Gupta <18guptayash@gmail.com>
2021-03-09 23:07:09 +0530

    updates

--- Competive/codechef long/interestingXOR.cpp
+++ Competive/codechef long/interestingXOR.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#define ll  long long
+#include <cmath>
+#include <utility>
+
+#define ll  long long int
 using namespace std;
 
 int main()
@@ -10,9 +13,19 @@ int main()
     while(tc--){
         ll C;
         cin>>C;
-        
-
+        ll dpow=1;
+        ll i=0;
+        while(true){
+        	dpow=pow(2,i);
+        	if(dpow>C) break;
+        	else i++;
+        	
+        }
+        ll p = (ll)(log2(C));
+        ll A, B;
+        ll mask = 1 << p; 
+    	A= (A & ~mask) | ((1 << p) & mask); 
+    	B=(A & ~mask) | ((0 << p) & mask); 
     }
-
     return 0;
 }
